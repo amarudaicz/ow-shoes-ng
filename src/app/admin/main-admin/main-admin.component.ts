f85256a9f2f222736985bf5ac70690231519d25c
amaru <amarudaicz@gmail.com>
2023-01-16 13:53:17 -0300

    v1.0.0

--- src/app/admin/main-admin/main-admin.component.ts
+++ src/app/admin/main-admin/main-admin.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-admin',
   templateUrl: './main-admin.component.html',
-  styleUrls: ['./main-admin.component.css']
+  styleUrls: ['./main-admin.component.scss']
 })
 export class MainAdminComponent implements OnInit {
 

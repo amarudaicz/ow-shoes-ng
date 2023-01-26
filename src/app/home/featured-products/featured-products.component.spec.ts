49fb01dc88a3b2c4b815fb927b732bdaef26f628
amaru <amarudaicz@gmail.com>
2023-01-26 15:03:25 -0300

    terminando home

---
+++ src/app/home/featured-products/featured-products.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { FeaturedProductsComponent } from './featured-products.component';
+
+describe('FeaturedProductsComponent', () => {
+  let component: FeaturedProductsComponent;
+  let fixture: ComponentFixture<FeaturedProductsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FeaturedProductsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FeaturedProductsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
